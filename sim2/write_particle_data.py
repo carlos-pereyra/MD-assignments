8fa834c8a8f782b7d4510a7ed70e3d783dcf83c3
Carlos Pereyra <Revolver@Carloss-MacBook-Pro.local>
2019-05-07 23:15:41 -0700

    proper tree writing

--- sim2/write_particle_data.py
+++ sim2/write_particle_data.py
@@ -1,87 +1,21 @@
 """
 *  author: carlos p
-*  purpose: solve particle in a box
+*  purpose: solve n-particle verlet
 *
 """
 
 import numpy as np
 from numpy import random
-from datetime import datetime
 import ROOT
 from ROOT import TTree,TFile,gROOT
 from ROOT import TCanvas,TGraph,TPad,TBrowser
-
-gROOT.ProcessLine("struct Particle {\
-std::vector<float> t;\
-std::vector<float> ux;\
-std::vector<float> uy;\
-std::vector<float> x;\
-std::vector<float> y;\
-std::vector<float> fx;\
-std::vector<float> fy;\
-std::vector<float> vx;\
-std::vector<float> vy;\
-Float_t k;\
-Float_t m;\
-Float_t dt;\
-Int_t id;\
-std::vector<float> l_mv;\
-std::vector<float> u_wl;\
-std::vector<float> u_lj;\
-std::vector<float> total_u_per_atom;\
-std::vector<float> total_u;\
-std::vector<float> dtt;\
-};\
-class Box{\
-public:\
-std::vector<Particle> atoms;\
-};");
-
-from ROOT import Box
-box=Box()
+from datetime import datetime
+from array import array
 
 random.seed(int(datetime.now().strftime("%s")))
 l=20
-natoms=20
+natoms=10
 nsteps=10000
-nstore=8000
-dt=np.ones(natoms)*0.01
-
-#   STEP0:
-#        * Initial Conditions
-#        * Global Variables
-t=np.zeros(natoms)
-ux=np.zeros(natoms)
-uy=np.zeros(natoms)
-rx=random.rand(natoms)*l
-ry=random.rand(natoms)*l
-for i in range(0,len(rx)):
-    for j in range(i,len(rx)):
-        while (np.sqrt((rx[i]-rx[j])**2+(ry[i]-ry[j])**2)<1 and i!=j) or (np.sqrt((rx[i]-l)**2+(ry[i]-l)**2)<1):
-            print("too close together i {} j {}".format(i,j))
-            rx[i]=random.random()*l
-            ry[i]=random.random()*l
-
-fx=np.zeros(natoms)
-fy=np.zeros(natoms)
-vx=np.ones(natoms)*2
-vy=np.ones(natoms)*3
-#energy
-r=np.zeros((natoms,natoms))
-u_lj_mat=np.zeros((natoms,natoms))
-l_mv_n=np.zeros(natoms)
-u_wl_n=np.zeros(natoms)
-u_lj_n=np.zeros(natoms)
-u_per_atom=0
-u_per_time=0
-#system constants
-m=np.ones(natoms)
-k=np.zeros(natoms)
-#file out
-box.atoms.resize(natoms)
-file=TFile("data/particle_box_n{:d}_dt1_{:d}.root".format(natoms,int(1/dt[0])), "recreate" );
-tree=TTree("time_tree", "store_particle_data")
-tree.Branch('box',box)
 
 
 #   STEP1:
@@ -106,7 +40,7 @@ def wall_force(xi,nu):
     fx_left_boundary    = 12*((1/xi)**13-(1/xi)**7)
     fx_right_boundary   = -12*((1/(l-xi))**13-(1/(l-xi))**7)
     fx                  = fx_left_boundary+fx_right_boundary
-
+    
     fy_bottom_boundary  = 12*((1/nu)**13-(1/nu)**7)
     fy_top_boundary     = -12*((1/(l-nu))**13-(1/(l-nu))**7)
     fy                  = fy_bottom_boundary+fy_top_boundary
@@ -114,72 +48,136 @@ def wall_force(xi,nu):
 
 
 #   STEP2:
-#       * Verlet Algorithm + Energy
-#
-for n in range(0,nsteps):
-    l_mv_n=np.zeros(natoms)
-    u_wl_n=np.zeros(natoms)
-    u_lj_n=np.zeros(natoms)
-    potential_energy_over_time=0
-    for p in range(0,natoms):
-        ''' Lennard-Jones '''
-        for j in range(p,natoms): #interaction potential
-            r[p][j]=((rx[p]-rx[j])**2+(ry[p]-ry[j])**2)**1/2.
-            r[j][p]=r[p][j]
-            if p==j:
-                r[p][j]=0
-                u_lj_mat[p][j]=0
-            else:
-                u_lj_mat[p][j]=lennard_jones_potential(r[p][j])
-                u_lj_mat[j][p]=u_lj_mat[p][j]
-    
-        ''' Each Atom's Instantaneous Energy '''
-        u_lj=np.sum(u_lj_mat[p])
-        l_mv=0.5*m[p]*(vx[p]**2+vy[p]**2)
-        u_wl=wall_potential(rx[p],ry[p])
-        ''' Sum Energy for each Atom '''
-        u_lj_n[p]+=np.sum(u_lj_mat[p])
-        l_mv_n[p]+=l_mv
-        u_wl_n[p]+=u_wl
-        ''' Verlet-Method '''
-        t[p]=dt[p]*n
-        ux[p]=vx[p]+dt[p]*fx[p]/(2*m[p])
-        uy[p]=vy[p]+dt[p]*fy[p]/(2*m[p])
-        rx[p]=rx[p]+ux[p]*dt[p]
-        ry[p]=ry[p]+uy[p]*dt[p]
-        fx[p],fy[p]=wall_force(rx[p],ry[p])
-        vx[p]=ux[p]+dt[p]*fx[p]/(2*m[p])
-        vy[p]=uy[p]+dt[p]*fy[p]/(2*m[p])
-        
-        #triggering condition
-        if n<nstore:
-            box.atoms[p].t.push_back(t[p])
-            box.atoms[p].ux.push_back(ux[p])
-            box.atoms[p].uy.push_back(uy[p])
-            box.atoms[p].x.push_back(rx[p])
-            box.atoms[p].y.push_back(ry[p])
-            box.atoms[p].fx.push_back(fx[p])
-            box.atoms[p].fy.push_back(fy[p])
-            box.atoms[p].vx.push_back(vx[p])
-            box.atoms[p].vy.push_back(vy[p])
-            box.atoms[p].k=k[p]
-            box.atoms[p].m=m[p]
-            box.atoms[p].dt=dt[p]
-            box.atoms[p].id=p
-            box.atoms[p].l_mv.push_back(l_mv) #kinetic energy
-            box.atoms[p].u_wl.push_back(u_wl) #wall energy
-            box.atoms[p].u_lj.push_back(u_lj) #lennard jones energy
-            box.atoms[p].total_u_per_atom.push_back(l_mv+u_wl+u_lj)
-            potential_energy_over_time+=l_mv+u_wl+u_lj
+#       * Variables
+file=TFile("data/test.root", "recreate" );
+tree=TTree("time-sequence", "particle object storage")
 
-    if n==nstore:
-        box.atoms[p].total_u.push_back(potential_energy_over_time/(n+1))
-        box.atoms[p].dtt.push_back(dt[0])
+t_dum=array('f',[0]*natoms)
+ux_dum=array('f',[0]*natoms)
+uy_dum=array('f',[0]*natoms)
+x_dum=array('f',[0]*natoms)
+y_dum=array('f',[0]*natoms)
+fx_dum=array('f',[0]*natoms)
+fy_dum=array('f',[0]*natoms)
+vx_dum=array('f',[2]*natoms)
+vy_dum=array('f',[3]*natoms)
+dt_dum=array('f',[0.01]*natoms)
+m_dum=array('f',[1]*natoms)
+ke_dum=array('f',[0]*natoms)
+pe_wall_dum=array('f',[0]*natoms)
+pe_jone_dum=array('f',[0]*natoms)
+
+t=array('f',[0])
+ux=array('f',[0])
+uy=array('f',[0])
+x=array('f',[0])
+y=array('f',[0])
+fx=array('f',[0])
+fy=array('f',[0])
+vx=array('f',[0])
+vy=array('f',[0])
+id=array('i',[0])
+dt=array('f',[0])
+m=array('f',[1])
+ke=array('f',[0])
+pe_wall=array('f',[0])
+pe_jone=array('f',[0])
+tree.Branch('t',t,'t/F')
+tree.Branch('ux',ux,'ux/F')
+tree.Branch('uy',uy,'uy/F')
+tree.Branch('x',x,'x/F')
+tree.Branch('y',y,'y/F')
+tree.Branch('fx',fx,'fx/F')
+tree.Branch('fy',fy,'fy/F')
+tree.Branch('vx',vx,'vx/F')
+tree.Branch('vy',vy,'vy/F')
+
+tree.Branch('id',id,'id/I')
+tree.Branch('dt',dt,'dt/F')
+tree.Branch('m',m,'m/F')
+tree.Branch('ke',ke,'ke/F')
+tree.Branch('pe_wall',pe_wall,'pe_wall/F')
+tree.Branch('pe_jone',pe_jone,'pe_jone/F')
+
+for i in range(0,len(x_dum)):
+    for j in range(i,len(x_dum)):
+        while (np.sqrt((x_dum[i]-x_dum[j])**2+(y_dum[i]-y_dum[j])**2)<1) or (np.sqrt((x_dum[i]-l)**2+(y_dum[i]-l)**2)<1):
+            print("too close together i {} j {}".format(i,j))
+            x_dum[i]=random.random()*l
+            y_dum[i]=random.random()*l
+            if(i==j):
+                break
+
+for i in range(0,len(x_dum)):
+    for j in range(i,len(x_dum)):
+        while (np.sqrt((x_dum[i]-x_dum[j])**2+(y_dum[i]-y_dum[j])**2)<1) or (np.sqrt((x_dum[i]-l)**2+(y_dum[i]-l)**2)<1):
+            print("too close together i {} j {}".format(i,j))
+            x_dum[i]=random.random()*l
+            y_dum[i]=random.random()*l
+            if(i==j):
+                break
+
+r=np.zeros((natoms,natoms))
+u_lj_mat=np.zeros((natoms,natoms))
+
+
+#   STEP3:
+#       * Verlet Algorithm + Energy
+for inc in range(0,1):
+    dt_dum=array('f',[0.01*(inc+1)]*natoms)
+    for n in range(0,nsteps):
+        for p in range(0,natoms):
+            ''' Lennard-Jones '''
+            for j in range(p,natoms): #interaction potential
+                r[p][j]=((x_dum[p]-x_dum[j])**2+(y_dum[p]-y_dum[j])**2)**1/2.
+                r[j][p]=r[p][j]
+                if p==j:
+                    r[p][j]=0
+                    u_lj_mat[p][j]=0
+                else:
+                    u_lj_mat[p][j]=lennard_jones_potential(r[p][j])
+                    u_lj_mat[j][p]=u_lj_mat[p][j]
+        
+            ''' Each Atom's Instantaneous Energy '''
+            pe_jone_dum[p]=np.sum(u_lj_mat[p])
+            ke_dum[p]=0.5*m_dum[p]*(vx_dum[p]**2+vy_dum[p]**2)
+            pe_wall_dum[p]=wall_potential(x_dum[p],y_dum[p])
+            ''' Sum Energy for each Atom '''
+            #u_lj_n[p]+=np.sum(u_lj_mat[p])
+            #l_mv_n[p]+=l_mv
+            #u_wl_n[p]+=u_wl
+            ''' Verlet-Method '''
+            t_dum[p]=dt_dum[p]*n
+            ux_dum[p]=vx_dum[p]+dt_dum[p]*fx_dum[p]/(2*m_dum[p])
+            uy_dum[p]=vy_dum[p]+dt_dum[p]*fy_dum[p]/(2*m_dum[p])
+            x_dum[p]=x_dum[p]+ux_dum[p]*dt_dum[p]
+            y_dum[p]=y_dum[p]+uy_dum[p]*dt_dum[p]
+            fx_dum[p],fy_dum[p]=wall_force(x_dum[p],y_dum[p])
+            vx_dum[p]=ux_dum[p]+dt_dum[p]*fx_dum[p]/(2*m_dum[p])
+            vy_dum[p]=uy_dum[p]+dt_dum[p]*fy_dum[p]/(2*m_dum[p])
+
+            #triggering condition
+            if n<1000: #and ncycle[p]>0: # triggering condition
+                t[0]=t_dum[p]
+                ux[0]=ux_dum[p]
+                uy[0]=uy_dum[p]
+                x[0]=x_dum[p]
+                y[0]=y_dum[p]
+                fx[0]=fx_dum[p]
+                fy[0]=fy_dum[p]
+                vx[0]=vx_dum[p]
+                vy[0]=vy_dum[p]
+                id[0]=p
+                dt[0]=dt_dum[p]
+                ke[0]=ke_dum[p]
+                pe_wall[0]=pe_wall_dum[p]
+                pe_jone[0]=pe_jone_dum[p]
+                tree.Fill()
+
+        print("time: {}".format(t_dum[p]))
+
 
 #   STEP3:
 #       * write to disk
-tree.Fill()
 tree.Write()
 file.Close()
-
-#print("rx: {}, ry: {}, vx: {}, vy: {}, fx: {}, fy: {}, ux: {}, uy: {} id: {} dt: {} m: {}".format(rx[p],ry[p],vx[p],vy[p],fx[p],fy[p],ux[p],uy[p],p,dt[p],m[p]))
